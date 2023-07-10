9bf6210aa73faa020c281b8cfcea169ace8a9fbf
Danivaldo Sousa <danivaldo@gmail.com>
2023-07-09 21:08:41 -0300

    First commit

---
+++ build/print_bluetooth_thermal/generated/source/buildConfig/debug/app/web/groons/print_bluetooth_thermal/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.web.groons.print_bluetooth_thermal;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "app.web.groons.print_bluetooth_thermal";
+  public static final String BUILD_TYPE = "debug";
+}
